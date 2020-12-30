5d18d5b27d68fa9ffbb3c903e889877590195918
Andrews Patrick <47216578+andrews002@users.noreply.github.com>
2020-12-30 15:03:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ function MyKeyInfo(key) {
 async function test() {
   const pem = require("pem");
   const fs = require("fs");
-  const pfx = fs.readFileSync("C:\\Gescom\\agil.pfx");
+  const pfx = fs.readFileSync("C:\\Gescom\\certificado novo.pfx");
 
-  await pem.readPkcs12(pfx, { p12Password: "agil2061" }, async (err, cert) => {
+  await pem.readPkcs12(pfx, { p12Password: "35111439" }, async (err, cert) => {
     if (err) {
       throw err;
     }
@@ -34,7 +34,7 @@ async function test() {
     ];
 
     sig.addReference("//*[local-name(.)='PESQUISAR_ESTOQUE']", transforms);
-    sig.signingKey = fs.readFileSync(path.normalize("C:\\Gescom\\key.pem"));
+    sig.signingKey = cert;
     sig.canonicalizationAlgorithm =
       "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
 
@@ -44,7 +44,7 @@ async function test() {
       .replace(/\n/g, "")
       .replace(/\r/g, "");
 
-    console.log(certificado);
+    // console.log(certificado);
     sig.keyInfoProvider = new MyKeyInfo(certificado);
     sig.computeSignature(xml, {
       location: {
@@ -57,4 +57,30 @@ async function test() {
   });
 }
 
-test();
+async function testSoap() {
+  var url = "https://secweb.procergs.com.br/cdv/IntegracaoGidSoap";
+  const xml = fs.readFileSync(`${__dirname}\\signed.xml`, "utf8");
+  const soapRequest = require("easy-soap-request");
+  var convert = require("xml-js");
+
+  const sampleHeaders = {
+    "user-agent": "sampleTest",
+    "Content-Type": "text/xml;charset=UTF-8",
+    
+  };
+
+  (async () => {
+    const { response } = await soapRequest({
+      url: url,
+      headers: sampleHeaders,
+      xml: xml,
+    });
+    const { headers, body, statusCode } = response;
+
+    var result = convert.xml2json(body, { compact: true, spaces: 4 });
+    fs.writeFileSync(`${__dirname}\\res.json`, result);
+  })();
+}
+
+// test();
+testSoap();
